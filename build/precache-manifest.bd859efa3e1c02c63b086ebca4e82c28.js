204d04e55125f27f31c9cc5d3fd776332ed84b17
vanderson-henrique <vhbenedito@gmail.com>
2021-04-13 11:53:49 -0300

    Deploy projeto frontend-online-store

---
+++ build/precache-manifest.bd859efa3e1c02c63b086ebca4e82c28.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7e74900ec34a0428e4ec875e0ce4f7",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "b68c77b8d51c07a86300",
+    "url": "/frontend-online-store/static/css/main.bc0ba610.chunk.css"
+  },
+  {
+    "revision": "ea17f1a9fec72523a184",
+    "url": "/frontend-online-store/static/js/2.6d404e39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-online-store/static/js/2.6d404e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68c77b8d51c07a86300",
+    "url": "/frontend-online-store/static/js/main.12c87de8.chunk.js"
+  },
+  {
+    "revision": "666759cb5b12e2b0323c",
+    "url": "/frontend-online-store/static/js/runtime-main.d0c050e9.js"
+  },
+  {
+    "revision": "c77a1b6075a6b9e23b436b1d394aa8a6",
+    "url": "/frontend-online-store/static/media/Logo.c77a1b60.svg"
+  },
+  {
+    "revision": "af5956c4e689c082c45aa8e57f2911dd",
+    "url": "/frontend-online-store/static/media/kart.af5956c4.svg"
+  },
+  {
+    "revision": "a529e5ae4f917ad9b5f4c1f3c99e3e32",
+    "url": "/frontend-online-store/static/media/lupa-black.a529e5ae.svg"
+  }
+]);
