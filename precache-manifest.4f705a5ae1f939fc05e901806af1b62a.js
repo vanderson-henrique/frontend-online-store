7402349f0aa71c1907c772e21fafea2973915b87
vanderson-henrique <vhbenedito@gmail.com>
2021-04-13 12:46:27 -0300

    Updates

---
+++ precache-manifest.4f705a5ae1f939fc05e901806af1b62a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e5fe4f6f73d4fb795fe849b00ea5b6",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "8354bc6330d96aef2aad",
+    "url": "/frontend-online-store/static/css/main.bc0ba610.chunk.css"
+  },
+  {
+    "revision": "f537ec39d00269ba3891",
+    "url": "/frontend-online-store/static/js/2.ea208aba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-online-store/static/js/2.ea208aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8354bc6330d96aef2aad",
+    "url": "/frontend-online-store/static/js/main.94e2734f.chunk.js"
+  },
+  {
+    "revision": "666759cb5b12e2b0323c",
+    "url": "/frontend-online-store/static/js/runtime-main.d0c050e9.js"
+  },
+  {
+    "revision": "c77a1b6075a6b9e23b436b1d394aa8a6",
+    "url": "/frontend-online-store/static/media/Logo.c77a1b60.svg"
+  },
+  {
+    "revision": "af5956c4e689c082c45aa8e57f2911dd",
+    "url": "/frontend-online-store/static/media/kart.af5956c4.svg"
+  },
+  {
+    "revision": "a529e5ae4f917ad9b5f4c1f3c99e3e32",
+    "url": "/frontend-online-store/static/media/lupa-black.a529e5ae.svg"
+  }
+]);
